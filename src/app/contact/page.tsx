6107163878fa52a2eebe27dc17d5717c9d938ef3
T-I <tito40358@gmail.com>
2026-09-18 14:33:52 +0900

    フォーム微調整

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -39,6 +39,31 @@ export default function ContactPage() {
                 </div>
                 <div>
                     <label>
+                        該当URL
+                    </label>
+                    <input
+                        type="url"
+                        onChange={(e) =>
+                            alert
+                        }
+                        className="peer w-full px-3 py-2 rounded-lg border border-slate-300
+         focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 focus:outline-none" />
+                </div>
+                <div>
+                    <label>
+                        タイトル
+                    </label>
+                    <input
+                        type="text"
+                        required
+                        onChange={(e) =>
+                            alert
+                        }
+                        className="peer w-full px-3 py-2 rounded-lg border border-slate-300
+         focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 focus:outline-none" />
+                </div>
+                <div>
+                    <label>
                         本文
                     </label>
                     <textarea
